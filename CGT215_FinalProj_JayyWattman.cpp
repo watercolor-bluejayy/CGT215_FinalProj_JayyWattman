ebec6ae533bc971f71b50dc5d6254b4d063205bb
watercolor-bluejayy <footballgirl78000@gmail.com>
2024-04-16 16:44:59 -0400

    Add project files.

---
+++ CGT215_FinalProj_JayyWattman.cpp
@@ -0,0 +1,87 @@
+// CGT215_FinalProj_JayyWattman.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
+
+//keyboard speed constant, affects how much power each key press imparts on the cbow
+const float KB_Speed = 0.2; // magic number, might need to adjust
+
+//function for loading textures that can be referenced later
+void LoadTex(Texture& tex, string filename)
+{
+    if (!tex.loadFromFile(filename))
+    {
+        cout << "Could not load " << filename << endl;
+    }
+}
+
+//need keyboard controls function
+void MoveBar(PhysicsSprite& bar, int elapsedMS) 
+{
+    if (Keyboard::isKeyPressed(Keyboard::Right)) 
+    {
+        Vector2f newPos(bar.getCenter());
+        newPos.x = newPos.x + (KB_Speed * elapsedMS);
+        bar.setCenter(newPos);
+    }
+    if (Keyboard::isKeyPressed(Keyboard::Left)) 
+    {
+        Vector2f newPos(bar.getCenter());
+        newPos.x = newPos.x - (KB_Speed * elapsedMS);
+        bar.setCenter(newPos);
+    }
+}
+
+int main()
+{
+    RenderWindow window(VideoMode(800, 600), "BrickBreaker");
+    World world(Vector2f(0, 1)); //Gravity set to 1 so the ball will bounce
+    int score(0); //start at zero because you haven't earned any points yet
+    int lives(3); //start at 3 so it can decrement down
+
+    //make our walls, ceiling, and floor so the ball doesn't go flying off screen
+    PhysicsRectangle floor; 
+    floor.setSize(Vector2f(800, 20)); 
+    floor.setCenter(Vector2f(400, 590)); 
+    floor.setStatic(true); //makes sure that the floor doesn't move
+    world.AddPhysicsBody(floor); 
+
+    PhysicsRectangle LeftWall;
+    LeftWall.setSize(Vector2f(20, 560));
+    LeftWall.setCenter(Vector2f(790, 300));
+    LeftWall.setStatic(true);
+    world.AddPhysicsBody(LeftWall);
+
+    PhysicsRectangle RightWall;
+    RightWall.setSize(Vector2f(20, 560));
+    RightWall.setCenter(Vector2f(10, 300));
+    RightWall.setStatic(true);
+    world.AddPhysicsBody(RightWall);
+
+    PhysicsRectangle ceiling; 
+    ceiling.setSize(Vector2f(800, 20)); 
+    ceiling.setCenter(Vector2f(400, 10)); 
+    ceiling.setStatic(true); 
+    world.AddPhysicsBody(ceiling); 
+
+    //make the ball
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
